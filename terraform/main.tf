c8ce0b1d5af412979da5ebc461166187ae4b0981
iridon0920 <iridon0920@gmail.com>
2024-10-27 15:08:08 +0900

    Update main.tf
    Add: Terraformにて「ACM証明書の検証Route53レコード追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -311,6 +311,21 @@ resource "aws_acm_certificate" "cloudfront_cert" {
   validation_method = "DNS"
 }
 
+# CloudFront Certificate Validation
+resource "aws_route53_record" "cloudfront_cert_validation" {
+  name    = tolist(aws_acm_certificate.cloudfront_cert.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.cloudfront_cert.domain_validation_options)[0].resource_record_type
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  records = [tolist(aws_acm_certificate.cloudfront_cert.domain_validation_options)[0].resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cloudfront_cert" {
+  provider                = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.cloudfront_cert.arn
+  validation_record_fqdns = [aws_route53_record.cloudfront_cert_validation.fqdn]
+}
+
 # Route 53 Record
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
